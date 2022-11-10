055670d0b125666daf3485419fa9910afe47fa30
actuallyakshay <akshay.rajput1197@gmail.com>
2022-11-10 17:15:46 +0530

    added

---
+++ Frontend/src/App.js
@@ -0,0 +1,17 @@
+
+import React from "react";
+import AllProducts from "./Components/AllProduct/AllProducts";
+import CultStoreNavbar from "./Components/CultStoreNavbar/CultStoreNavbar";
+
+
+
+function App() {
+  return (
+    <div>
+      <CultStoreNavbar />
+      <AllProducts />
+    </div>
+  );
+}
+
+export default App;
